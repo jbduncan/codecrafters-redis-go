d885444bfe8a6b634ddf9b28659b6058924da7ab
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2024-03-24 23:24:56 +0000

    Implement the ECHO command

--- app/server.go
+++ app/server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -35,21 +39,186 @@ func main() {
 func handleConn(conn net.Conn) {
 	defer errorHandlingClose(conn)
 
-	for {
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			printErr(err)
-		}
+	connReader := bufio.NewReader(conn)
 
-		_, err = conn.Write([]byte("+PONG\r\n"))
+	for {
+		b, err := connReader.ReadByte()
 		if err != nil {
 			printErr(err)
+			break
+		}
+		switch b {
+		case '+':
+			err = processPingRequest(connReader, conn)
+			break
+		case '*':
+			err = processEchoRequest(connReader, conn)
+			break
+		default:
+			_, err := conn.Write([]byte("-ERR unknown command\r\n"))
+			if err != nil {
+				printErr(err)
+			}
+		}
+		if err != nil {
+			printErr(err)
+			// TODO: Respond with Redis error message
 		}
 	}
+}
+
+func processPingRequest(connReader *bufio.Reader, connWriter io.Writer) error {
+	// TODO: Is draining connReader even needed?
+	_, err := io.Copy(io.Discard, connReader)
+	if err != nil {
+		return err
+	}
+
+	_, err = connWriter.Write([]byte("+PONG\r\n"))
+	return err
+}
+
+func processEchoRequest(connReader *bufio.Reader, connWriter io.Writer) error {
+	array, err := parseArray(connReader)
+	if err != nil {
+		return err
+	}
+
+	if len(array) == 0 {
+		return errors.New("incomplete request")
+	}
+	if !strings.EqualFold(array[0], "ECHO") {
+		return errors.New("unrecognized command")
+	}
+	if len(array) != 2 {
+		return errors.New("ECHO command expected to have one argument")
+	}
+
+	echo := array[1]
+	response := fmt.Sprintf("$%d\r\n%s\r\n", len(echo), echo)
+	_, err = connWriter.Write([]byte(response))
+	return err
+}
+
+func parseArray(reader *bufio.Reader) ([]string, error) {
+	arrayLength, err := readUnsignedInt(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	err = expectCRLF(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	var array []string
+	for i := 0; i < arrayLength; i++ {
+		elem, err := readBulkString(reader)
+		if err != nil {
+			return nil, err
+		}
+		array = append(array, elem)
+	}
+
+	return array, nil
+}
+
+func readUnsignedInt(reader *bufio.Reader) (int, error) {
+	var buffer bytes.Buffer
+
+	b, err := readDigit(reader)
+	if err != nil {
+		// There were no digits at all, so return early
+		return 0, err
+	}
+
+	_ = buffer.WriteByte(b)
+
+	for {
+		b, err := readDigit(reader)
+		if err != nil {
+			// At least one digit was read, so stop
+			break
+		}
+
+		_ = buffer.WriteByte(b)
+	}
+
+	return strconv.Atoi(buffer.String())
+}
+
+func readDigit(reader *bufio.Reader) (byte, error) {
+	bs, err := reader.Peek(1)
+	if err != nil {
+		return 0, err
+	}
+	if !('0' <= bs[0] && bs[0] <= '9') {
+		return 0, errors.New("expected digit")
+	}
+	b, err := reader.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	return b, nil
+}
+
+func readBulkString(reader *bufio.Reader) (string, error) {
+	err := expect(reader, '$')
+	if err != nil {
+		return "", err
+	}
+
+	stringLength, err := readUnsignedInt(reader)
+	if err != nil {
+		return "", err
+	}
+
+	err = expectCRLF(reader)
+	if err != nil {
+		return "", err
+	}
+
+	var builder strings.Builder
+	for i := 0; i < stringLength; i++ {
+		b, err := reader.ReadByte()
+		if err != nil {
+			return "", err
+		}
+		builder.WriteByte(b)
+	}
+
+	err = expectCRLF(reader)
+	if err != nil {
+		return "", err
+	}
+
+	return builder.String(), nil
+}
+
+func expectCRLF(reader *bufio.Reader) error {
+	err := expect(reader, '\r')
+	if err != nil {
+		return err
+	}
+	err = expect(reader, '\n')
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func expect(reader *bufio.Reader, b byte) error {
+	readBytes, err := reader.Peek(1)
+	if err != nil {
+		return err
+	}
+
+	if readBytes[0] != b {
+		return fmt.Errorf("expected %v but was %v", b, readBytes[0])
+	}
+
+	_, err = reader.ReadByte()
+	return err
 }
 
 func printErr(err error) {
