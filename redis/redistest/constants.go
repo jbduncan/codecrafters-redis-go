7f2317e8927056110cdc5369fb4d105988d03805
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2025-08-21 11:50:24 +0100

    Address a TODO

--- redis/redistest/constants.go
+++ redis/redistest/constants.go
@@ -1,21 +1,60 @@
 package redistest
 
-// TODO: Consider replacing these consts with helper functions that use
-//       RESP2Writer behind the scenes, e.g.,
-//   - bstrs(...string) -> string repr of array of bulk strings
-//   - sstr(string) -> string repr of single simple string
-const (
-	RequestPingLowercase     = "*1\r\n$4\r\nping\r\n"
-	RequestPingUppercase     = "*1\r\n$4\r\nPING\r\n"
-	RequestEchoLowercaseFoo  = "*2\r\n$4\r\necho\r\n$3\r\nfoo\r\n"
-	RequestEchoUppercaseQuux = "*2\r\n$4\r\nECHO\r\n$4\r\nquux\r\n"
-	RequestSetLowercaseFoo   = "*2\r\n$3\r\nset\r\n$3\r\nfoo\r\n"
-	RequestGetLowercaseFoo   = "*2\r\n$3\r\nget\r\n$3\r\nfoo\r\n"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/codecrafters-io/redis-starter-go/redis"
+)
+
+var (
+	RequestPingLowercase     = bstrs("ping")
+	RequestPingUppercase     = bstrs("PING")
+	RequestEchoLowercaseFoo  = bstrs("echo", "foo")
+	RequestEchoUppercaseQuux = bstrs("ECHO", "quux")
+	RequestSetLowercaseFoo   = bstrs("set", "foo")
+	RequestGetLowercaseFoo   = bstrs("get", "foo")
 	RequestInvalidSyntax     = "*1\r\n^"
 
-	ResponsePong               = "+PONG\r\n"
-	ResponseFoo                = "$3\r\nfoo\r\n"
-	ResponseQuux               = "$4\r\nquux\r\n"
-	ResponseOK                 = "+OK\r\n"
-	ResponseInvalidSyntaxError = "!41\r\nERR Protocol error: expected '$', got '^'\r\n"
+	ResponsePong               = sstr("PONG")
+	ResponseFoo                = bstr("foo")
+	ResponseQuux               = bstr("quux")
+	ResponseOK                 = sstr("OK")
+	ResponseInvalidSyntaxError = berr("ERR Protocol error: expected '$', got '^'")
 )
+
+func bstr(value string) string {
+	var s strings.Builder
+	mustWrite(&s, redis.BulkString(value))
+	return s.String()
+}
+
+func bstrs(first string, rest ...string) string {
+	array := make(redis.Array, 0, len(rest)+1)
+	array = append(array, redis.BulkString(first))
+	for _, value := range rest {
+		array = append(array, redis.BulkString(value))
+	}
+
+	var s strings.Builder
+	mustWrite(&s, array)
+	return s.String()
+}
+
+func sstr(value string) string {
+	var s strings.Builder
+	mustWrite(&s, redis.SimpleString(value))
+	return s.String()
+}
+
+func berr(value string) string {
+	var s strings.Builder
+	mustWrite(&s, redis.MakeBulkError(value))
+	return s.String()
+}
+
+func mustWrite(s *strings.Builder, value redis.Value) {
+	if err := redis.NewRESP2Writer(s).Write(value); err != nil {
+		panic(fmt.Sprintf("unexpected RESP2 write error: %v", err))
+	}
+}
